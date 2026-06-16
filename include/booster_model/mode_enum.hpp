f97881abbd5b07ec370433fcbf25337f27a1bcf0
shikajiko <riyanfadli675@gmail.com>
2026-06-16 21:23:28 +0700

    add robot mode

---
+++ include/booster_model/mode_enum.hpp
@@ -0,0 +1,18 @@
+namespace booster_model::mode
+{
+
+enum class RobotMode 
+{
+    DAMP = 0,
+    PREP = 1,
+    WALK = 2,
+    CUSTOM = 3
+};
+
+enum class UpperControl
+{
+    OFF = 0,
+    ON = 1
+};
+
+}
